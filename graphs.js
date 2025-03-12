91435a62187e9f9102cfed170cc51f159c722bf5
kovacsannabella <kovacs.annabella@students.jedlik.eu>
2025-03-12 21:50:39 +0100

    when no data, graph hidden, message showing, but does not work entirely

--- graphs.js
+++ graphs.js
@@ -61,6 +61,21 @@ function makeTemperatureGraph(times, temperatures) {
  */
 function makePrecipitationGraph(times, precipitations) {
     const timeValues = times.map(time => new Date(time).toLocaleTimeString("hu-HU", { hour: "numeric", minute: "2-digit" }));
+    const hasPrecipitation = precipitations.some(p => p > 0);
+    
+    if(hasPrecipitation){
+        document.getElementById("precipitation_diagram").style.display = "none"; 
+        document.getElementById("no-precipitation-message").style.display = "block"; 
+        return;
+    }
+    else{
+        document.getElementById("precipitation_diagram").style.display = "block";
+        document.getElementById("no-precipitation-message").style.display = "none";
+    }
+
+    if (precipitationChartInstance) {
+        precipitationChartInstance.destroy();
+    }
 
     new Chart("precipitation-chart", {
         type: "bar",
