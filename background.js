bcb48748d2a90e8abab9d53481000e84e1d448c7
simonadamgyula <simon.adam.gyula@students.jedlik.eu>
2025-03-13 11:09:08 +0100

    fixed weather code handling

--- background.js
+++ background.js
@@ -4,43 +4,42 @@ function WeatherIcon(weatherCode) {
     const background = document.querySelector("body");
     let isNight = false;
 
-    console.log(weather_icon);
-    if (weatherCode==0 && today.getHours() >= 18 || weatherCode == 0 && today.getHours() < 5) {
+    if (weatherCode == 0 && today.getHours() >= 18 || weatherCode == 0 && today.getHours() < 5) {
         weather_icon.src = "pictures/ready_for_use/hold.png";
         isNight = true;
         background.style.backgroundImage = "url('pictures/ready_for_use/hold.png')";
     }
-    if (weatherCode==0) {
-        weather_icon.src = "pictures/ready_for_use/nap.png"; 
-        background.style.backgroundImage = `url('pictures/ready_for_use/${isNight ? "night_" : "day_" }clear.png')`;
-    } else if (weatherCode==28||40<=weatherCode<=49) {
+    if (weatherCode == 0) {
+        weather_icon.src = "pictures/ready_for_use/nap.png";
+        background.style.backgroundImage = `url('pictures/ready_for_use/${isNight ? "night_" : "day_"}clear.png')`;
+    } else if (weatherCode == 28 || 40 <= weatherCode && weatherCode <= 49) {
         weather_icon.src = "pictures/ready_for_use/cloud.png";
-        background.style.backgroundImage = `url('pictures/ready_for_use/${isNight ? "night_" : "day_" }cloudy.png')`;
-    } else if (weatherCode==20 || weatherCode==21 || weatherCode==23 || weatherCode==24 || 50<=weatherCode<=69) {
-        background.style.backgroundImage = `url('pictures/ready_for_use/${isNight ? "night_" : "day_" }rain.png')`;
+        background.style.backgroundImage = `url('pictures/ready_for_use/${isNight ? "night_" : "day_"}cloudy.png')`;
+    } else if (weatherCode == 20 || weatherCode == 21 || weatherCode == 23 || weatherCode == 24 || 50 <= weatherCode && weatherCode <= 69) {
+        background.style.backgroundImage = `url('pictures/ready_for_use/${isNight ? "night_" : "day_"}rain.png')`;
         weather_icon.src = "pictures/ready_for_use/rain.png";
-    } else if (weatherCode==25||weatherCode==28||80<=weatherCode<=99) {
+    } else if (weatherCode == 25 || weatherCode == 28 || 80 <= weatherCode && weatherCode <= 99) {
         weather_icon.src = "pictures/ready_for_use/storm.png";
-        background.style.backgroundImage = `url('pictures/ready_for_use/${isNight ? "night_" : "day_" }storm.png')`;
-    } else if (weatherCode==26||weatherCode==27||70<=weatherCode<=79) {
+        background.style.backgroundImage = `url('pictures/ready_for_use/${isNight ? "night_" : "day_"}storm.png')`;
+    } else if (weatherCode == 26 || weatherCode == 27 || 70 <= weatherCode && weatherCode <= 79) {
         weather_icon.src = "pictures/ready_for_use/snow.png";
-        background.style.backgroundImage = `url('pictures/ready_for_use/${isNight ? "night_" : "day_" }snow.png')`;
+        background.style.backgroundImage = `url('pictures/ready_for_use/${isNight ? "night_" : "day_"}snow.png')`;
     } else {
         weather_icon.src = "pictures/ready_for_use/nap.png";
-        background.style.backgroundImage = `url('pictures/ready_for_use/${isNight ? "night_" : "day_" }clear.png')`;
+        background.style.backgroundImage = `url('pictures/ready_for_use/${isNight ? "night_" : "day_"}clear.png')`;
     }
 }
 
 function iconByWeatherCode(weatherCode) {
-    if (weatherCode==0) {
-        return "pictures/ready_for_use/nap.png"; 
-    } else if (weatherCode==28||40<=weatherCode<=49) {
+    if (weatherCode == 0) {
+        return "pictures/ready_for_use/nap.png";
+    } else if (weatherCode == 28 || 40 <= weatherCode <= 49) {
         return "pictures/ready_for_use/cloud.png";
-    } else if (weatherCode==20 || weatherCode==21 || weatherCode==23 || weatherCode==24 || 50<=weatherCode<=69) {
+    } else if (weatherCode == 20 || weatherCode == 21 || weatherCode == 23 || weatherCode == 24 || 50 <= weatherCode <= 69) {
         return "pictures/ready_for_use/rain.png";
-    } else if (weatherCode==25||weatherCode==28||80<=weatherCode<=99) {
+    } else if (weatherCode == 25 || weatherCode == 28 || 80 <= weatherCode <= 99) {
         return "pictures/ready_for_use/storm.png";
-    } else if (weatherCode==26||weatherCode==27||70<=weatherCode<=79) {
+    } else if (weatherCode == 26 || weatherCode == 27 || 70 <= weatherCode <= 79) {
         return "pictures/ready_for_use/snow.png";
     } else {
         return "pictures/ready_for_use/nap.png";
